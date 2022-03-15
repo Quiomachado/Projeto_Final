ff0f80259a8bd3f5f2ecbddb47817c0222b95b47
Quiomachado <henriquevmachado@tecnico.ulisboa.pt>
2022-03-15 17:50:37 +0000

    update

--- P2/E2/main.c
+++ P2/E2/main.c
@@ -6,28 +6,29 @@ int main(){
     int mes;
     printf("Introduza o numero do mes: ");
     scanf("%i", &mes);
-    switch (mes) {
-        case 1 :
-        case 3 :
-        case 5 :
-        case 7 :
-        case 8 :
-        case 10:
-        case 12:
-            printf("O mes tem 31 dias.");
-            break;
-        case 4 :
-        case 6 :
-        case 9 :
-        case 11:
-            printf("O mes tem 30 dias.");
-            break;
-        case 2 :
-            printf("O mes tem 29 dias.");
-            break;
-        default:
-        printf("Introduza um valor entre 1 e 12 para tentar de novo ou um valor fora desse intervalo para fechar.");
-        scanf("%d", &mes);
+    do{
+        switch (mes) {
+            case 1 :
+            case 3 :
+            case 5 :
+            case 7 :
+            case 8 :
+            case 10:
+            case 12:
+                printf("O mes %d tem 31 dias.\n", mes);
+                break;
+            case 4 :
+            case 6 :
+            case 9 :
+            case 11:
+                printf("O mes %d tem 30 dias.\n", mes);
+                break;
+            case 2 :
+                printf("O mes %d tem 29 dias.\n", mes);
+                break;
         }
+            printf("Introduza um valor entre 1 e 12 para tentar de novo ou um valor fora desse intervalo para terminar: \n");
+            scanf("%d", &mes);
+    }while ((mes<=12)&&(mes>=1));
     return 0;
 }
