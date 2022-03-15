602b6627e86fbbe3c4055269ba48bab6204317a1
Quiomachado <henriquevmachado@tecnico.ulisboa.pt>
2022-03-15 15:31:24 +0000

    update

---
+++ P2/E1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+    int num, i;
+    double r;
+    i=1;
+    printf("Introduza o seu numero: ");
+    scanf("%d", &num);
+    while(num>=i){
+        r= num%i;
+        if(r==0){
+            printf("%d ", i);
+        }
+    i++;
+    }
+}
