8735cd1bcf27b6ca5fe7d2c22fbdb80adeb10e19
Quiomachado <henriquevmachado@tecnico.ulisboa.pt>
2022-03-17 01:38:54 +0000

    update

--- L3/E2/main.c
+++ L3/E2/main.c
@@ -2,7 +2,19 @@
 #include <stdlib.h>
 #include <math.h>
 
-
 int main(){
-   
+   int i, k, n, space;
+   k = 0;
+   printf("Introduza n: ");
+   scanf("%d", &n);
+   for(i=1;i<=n;++i, k=0){
+       for(space=1; space <= n-i; ++space){
+           printf("  ");
+       }
+       while(k != 2 * i - 1){
+           printf("* ");
+            ++k;
+       }
+       printf("\n");
+   }
 }
