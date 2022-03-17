e5a82b05ecb781e8746b461f6377a76b7a655c2b
Quiomachado <henriquevmachado@tecnico.ulisboa.pt>
2022-03-17 02:09:58 +0000

    update

--- L3/E2/main.c
+++ L3/E2/main.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(){
+int factorial(int x);
+int comb(int x, int y);
+
+int main()
+{
    int i, k, n, space;
    k = 0;
    printf("Introduza n: ");
@@ -12,9 +16,25 @@ int main(){
            printf("  ");
        }
        while(k != 2 * i - 1){
-           printf("* ");
+           printf("%d ", comb(i,k));
             ++k;
        }
        printf("\n");
    }
+}
+
+int factorial(int x)
+{
+    int i, num=1;
+    for(i=1;i<=x;i++){
+        num*=i;
+    }
+    return num;
+}
+
+int comb(int n, int k)
+{
+    int z;
+    z=factorial(n)/(factorial(k)*factorial(n-k));
+    return z;
 }
