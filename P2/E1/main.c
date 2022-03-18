067e975474f28cbe7541334f4dd6b616c66dcce4
Quiomachado <henriquevmachado@tecnico.ulisboa.pt>
2022-03-18 15:20:33 +0000

    update

--- P2/E1/main.c
+++ P2/E1/main.c
@@ -4,15 +4,20 @@
 
 int main (){
     int num, i;
-    double r;
+    double r, med, som, div;
     i=1;
+    div = 0;
     printf("Introduza o seu numero: ");
     scanf("%d", &num);
     while(num>=i){
         r = num%i;
         if(r==0){
             printf("%d ", i);
+            som += i;
+            div++;
         }
     i++;
     }
+    med=(double)som/div;
+    printf("\nA media e: %.3f", med);
 }
