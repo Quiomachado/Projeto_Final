8ed1e51b85c3b71dbe9ad7cfa07b22a6b17e82a4
Quiomachado <henriquevmachado@tecnico.ulisboa.pt>
2022-03-20 12:51:32 +0000

    update

--- L3/E3/main.c
+++ L3/E3/main.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+
+int calc_hist(int vect[]);
+int main (){
+    int vect[]= {0, 5, 2, 400};
+    printf("%d", calc_hist(vect));
+    return EXIT_SUCCESS;
+}
+
+int calc_hist(int vect[]){
+    int i;
+    for(i = 0; i < sizeof(vect); i++){
+    }
+    return i;
+}
