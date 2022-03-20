37dd3e79f6e84a9c6b894e33a226c8a7ce38bb5f
Quiomachado <henriquevmachado@tecnico.ulisboa.pt>
2022-03-20 17:09:32 +0000

    update

--- L3/E4/main.c
+++ L3/E4/main.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define MAX_SIZE 256
 
-void ini_vector();
+int *ini_vector(int n);
+void str_dig(int n, int vect[]);
 int main(){
+    int n;
+    printf("Introduza quantos numeros inteiros prentende introduzir:\n");
+    scanf("%d", &n);
+    int* vector;
+    vector = ini_vector(n);
+    str_dig(n, vector);
     return EXIT_SUCCESS;
 }
 
-void ini_vector(){
-    int n, i;
-    printf("Introduza quantos numeros inteiros pretende introduzir: \n");
-    scanf("%d", &n);
-    int vect[n];
+int *ini_vector(int n){
+    int i;
+    static int* vect;
+    vect = malloc(MAX_SIZE * sizeof(int));
     for(i=0; i<n; i++){
         printf("Introduza o %d valor:\n", i+1);
         scanf("%d", &vect[i]);
-        if(vect[i]<0 || vect[i]>9){
+        if((vect[i]<0) || (vect[i]>9)){
             printf("Introduza um valor entre 1 e 9:\n");
             scanf("%d", &vect[i]);
         }
     }
+    return vect;
 }
 
+void coder(int n, int vect[]){
+    int str[n];
+    int i,j;
+    char awnser[n];
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            
+        }
+    }
+}
