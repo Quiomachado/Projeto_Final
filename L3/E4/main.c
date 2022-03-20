1f48ac06e1d021876a03e75abcceeeb61f895c46
Quiomachado <henriquevmachado@tecnico.ulisboa.pt>
2022-03-20 14:32:34 +0000

    update

--- L3/E4/main.c
+++ L3/E4/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void ini_vector();
 int main(){
-    
     return EXIT_SUCCESS;
 }
+
+void ini_vector(){
+    int n, i;
+    printf("Introduza quantos numeros inteiros pretende introduzir: \n");
+    scanf("%d", &n);
+    int vect[n];
+    for(i=0; i<n; i++){
+        printf("Introduza o %d valor:\n", i+1);
+        scanf("%d", &vect[i]);
+        if(vect[i]<0 || vect[i]>9){
+            printf("Introduza um valor entre 1 e 9:\n");
+            scanf("%d", &vect[i]);
+        }
+    }
+}
+
