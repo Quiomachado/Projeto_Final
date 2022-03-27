afed5924a11c0725561802ba1efc087aa60dc514
Quiomachado <henriquevmachado@tecnico.ulisboa.pt>
2022-03-27 23:23:46 +0100

    update

--- Projeto/Henrique/main.c
+++ Projeto/Henrique/main.c
@@ -11,11 +11,11 @@ int main(){
     char tabuleiro[16][16];
     int eixonum[16];
     char eixoletras[16];
-    int n;
+    int n, scan;
     printf("Introduza o numero de linhas e colunas, este numero deve ser impar e estar entre 7 e 15:\n");
     scanf("%d", &n);
     while(n<7 || n>15 || (n%2)==0){
-        printf("Nao introduzio um valor valido, introduza um valor impar entre 7 e 15:\n");
+        printf("Nao introduzio um valor valido, introduza um valor impar entre 7 e 15: ");
         scanf("%d ", &n);
     }
     initablueiro(tabuleiro, eixoletras, eixonum, n);
@@ -79,26 +79,83 @@ void printabuleiro(char tabuleiro[16][16],char eixoletras[16], int eixonum[16], 
 }
 
 void editabuleiro(char tabuleiro[16][16], int n){
-    int i, k, x, y, dir;
+    int i=3, k, x, x2=0, y, dir, len, len_palavra, count=-1, multi=1, tempx, tempy;
     char input[25];
     scanf("%s", input);
+    len=strlen(input);
+    len_palavra=len-3;
     x=input[1]-'0';
     y=input[0]-'@';
     if(y>26){
         y=input[0]-'`';
     }
     dir=input[2]-'@';
-    for(i=3;i<=n;i++){
-        if(dir==8 || dir==40){
-            while(y<=n){
+    if(dir<0){
+        x2=(input[2]-'0')*10;
+        x=x+x2;
+        dir=input[3]-'@';
+        len_palavra=len-4;
+        i=4;
+    }
+    tempy=y;
+    tempx=x;
+    if(dir==8 || dir==40){
+        if(len_palavra>n-y){
+            printf("Introduzio uma palavra demasiado grande.");
+        }else{
+            while(y<=n,i<=len){
+                if((((y==(n/2))||y==((n/2)+2))&&(x==2 || x==n-1))||(((x==(n/2))||x==((n/2)+2))&&(y==2 || y==n-1))){
+                    printf("Esta palavra usa um espaco proibido!\n");
+                    while(y>=tempy){
+                        tabuleiro[x][y]='.';
+                        if((((y==(n/2))||y==((n/2)+2))&&(x==2 || x==n-1))||(((x==(n/2))||x==((n/2)+2))&&(y==2 || y==n-1))){
+                            tabuleiro[x][y]='#';
+                        }
+                        y--;
+                    }
+                    break;
+                }
                 tabuleiro[x][y]=input[i];
-                y++;
+                if(x==1 && y==1 || x==1 && y==n || x==n && y==1 || x==n && y==n){
+                    multi++;
+                }else if(i==k || n-i==k-1){
+                    count++;
+                }else if(((y==(n/2)+1)&&(x==1 || x==n))||((x==(n/2)+1)&&(y==1 || y==n))){
+                    count+=2;
+                }
+                count++;
+                y++, i++;
             }
-        }else if(dir==22 || dir==54){
-            while(x<=n){
+        }
+    }else if(dir==22 || dir==54){
+        if(len_palavra>n-x){
+            printf("Introduzio uma palavra demasiado grande.");
+        }else{
+            while(x<=n,i<=len_palavra){
+                if((((y==(n/2))||y==((n/2)+2))&&(x==2 || x==n-1))||(((x==(n/2))||x==((n/2)+2))&&(y==2 || y==n-1))){
+                    printf("Esta palavra usa um espaco proibido!\n");
+                    while(x>=tempx){
+                        tabuleiro[x][y]='.';
+                        if((((y==(n/2))||y==((n/2)+2))&&(x==2 || x==n-1))||(((x==(n/2))||x==((n/2)+2))&&(y==2 || y==n-1))){
+                            tabuleiro[x][y]='#';
+                        }
+                        x--;
+                    }
+                    break;
+                }
                 tabuleiro[x][y]=input[i];
-                x++;
+                if(x==1 && y==1 || x==1 && y==n || x==n && y==1 || x==n && y==n){
+                    multi++;
+                }else if(i==k || n-i==k-1){
+                    count++;
+                }else if(((y==(n/2)+1)&&(x==1 || x==n))||((x==(n/2)+1)&&(y==1 || y==n))){
+                    count+=2;
+                }
+                count++;
+                x++, i++;
             }
         }
     }
+    count*=multi;
+    printf("Essa jogada valeu %d pontos!", count);
 }
