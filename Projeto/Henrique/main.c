3b62fbfdb3492317f8a5bb8129855c7ab705fdd8
Quiomachado <henriquevmachado@tecnico.ulisboa.pt>
2022-03-27 18:24:42 +0100

    update

--- Projeto/Henrique/main.c
+++ Projeto/Henrique/main.c
@@ -10,7 +10,7 @@ int main(){
     int n;
     printf("Introduza o numero de linhas e colunas, este numero deve ser impar e estar entre 7 e 15:\n");
     scanf("%d", &n);
-    while((n<7 || n>15) || ((n%2)==0)){
+    while(n<7 || n>15 || (n%2)==0){
         printf("Nao introduzio um valor valido, introduza um valor impar entre 7 e 15:\n");
         scanf("%d", &n);
     }
@@ -19,32 +19,50 @@ int main(){
 
 void initablueiro(char tabuleiro[16][16], int n){
     int i, k;
-    for(i=0;i<=n+1;i++){
-        for(k=0;k<=n+1;k++){
+    int eixonum[n];
+    char eixoletras[n];
+    for(i=1;i<=n;i++){
+        for(k=1;k<=n;k++){
             tabuleiro[i][k]='.';
         }
     }
-    for(i=0;i<n;i++){
-        tabuleiro[i][0]= i +'1';
-        if(i+'1'>=58){
-            tabuleiro[i][0]='i';
-        }
+    for(i=0;i<=n;i++){
+        eixonum[i]=i;
+        eixoletras[i]=i+'@';
     }
     for(i=1;i<=n;i++){
-        tabuleiro[n][i]=i+'@';
-    }
-    for(i=0;i<=n+1;i++){
-        for(k=1;k<=n+1;k++){
-            if(i+1==k || n-i==k){
+        for(k=1;k<=n;k++){
+            if(i==k || n-i==k-1){
                 tabuleiro[i][k]='2';
             }
-
+            if(i==1 && k==1 || i==1 && k==n || i==n && k==1 || i==n && k==n){
+                tabuleiro[i][k]='$';
+            }
+            if(((k==(n/2)+1)&&(i==1 || i==n))||((i==(n/2)+1)&&(k==1 || k==n))){
+                tabuleiro[i][k]='3';
+            }
+            if((((k==(n/2))||k==((n/2)+2))&&(i==2 || i==n-1))||(((i==(n/2))||i==((n/2)+2))&&(k==2 || k==n-1))){
+                tabuleiro[i][k]='#';
+            }
         }
     }
-    for(i=0;i<=n;i++){
-        for(k=0;k<=n;k++){
+    /*for(i=1;i<=n;i++){
+        if(i<=n){
+            if(i<=9){
+                printf(" %d ", eixonum[i]);
+            }else{
+                printf("%d ", eixonum[i]);
+            }
+        }
+        for(k=1;k<=n;k++){
             printf("%c ", tabuleiro[i][k]);
         }
         printf("\n");
     }
+    for(i=n;i<=n;i++){
+        printf("   ");
+        for(k=1;k<=n;k++){
+            printf("%c ", eixoletras[k]);
+        }
+    }*/
 }
